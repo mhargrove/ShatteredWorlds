ba1d92f772745404ddafd898b222fa5c8e8c173b
Jack Weinnig <jackweinnig@gmail.com>
2015-02-04 19:50:27 -0600

    added shattereddreams

---
+++ Prototype/shattereddreams/shattereddreams.ino
@@ -0,0 +1,67 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+#define OUTPUT_READABLE_ACCELGYRO
+
+#define LED_PIN 13
+bool blinkState = false;
+
+
+void setup() {
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+    
+    //recommended baud 
+    Serial.begin(9600);
+
+    // initialize acc device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify accel connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    
+
+    }
+    
+void loop() {
+    // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+    int sensorValue = analogRead(A0);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+        Serial.print("a/g:\t");
+        Serial.print(ax); Serial.print("\t");
+        Serial.print(ay); Serial.print("\t");
+        Serial.print(az); Serial.print("\t");
+        if (sensorValue >= 100) {
+          Serial.println(1);
+        }
+        if (sensorValue == 0) {
+          Serial.println(0);
+        }
+        delay(1000); 
+//        Serial.print(gx); Serial.print("\t");
+//        Serial.print(gy); Serial.print("\t");
+//        Serial.println(gz);
+    #endif
+
+
+}
