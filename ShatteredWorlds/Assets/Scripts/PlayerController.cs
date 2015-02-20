6076ee6e57a5680fd175575f21b52aeb74a7a362
Colin Blaise <cblaise@60-f8-1d-c2-85-f8.wlan.lsu.edu>
2015-02-19 20:06:32 -0600

    Adds some materials from Trill and starts on level1 manager

--- ShatteredWorlds/Assets/Scripts/PlayerController.cs
+++ ShatteredWorlds/Assets/Scripts/PlayerController.cs
@@ -8,16 +8,20 @@ public class PlayerController : MonoBehaviour {
 
 	public Arduino arduino;
 	public GameObject gameController;
+
 	void Start()
 	{
-		gameController = GameObject.Find( "Game Controller" );
+		gameController = GameObject.Find( "GameController" );
 		if (gameController)
 		    arduino = gameController.GetComponent<GameController>().arduino;
 	}
 
 	void Update()
-	{
-
+	{  
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal * 10.0f, 0.0f, moveVertical * 10.0f);
+		rigidbody.AddForce (movement);
 	}
 
 }
