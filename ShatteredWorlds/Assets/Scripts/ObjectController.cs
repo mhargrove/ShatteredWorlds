ce75989c81c8e2b6817aaf819ffa93a0433f0afb
Morgan Hargrove <mhargr5@lsu.edu>
2015-03-03 15:42:25 -0600

    add basic UI

--- ShatteredWorlds/Assets/Scripts/ObjectController.cs
+++ ShatteredWorlds/Assets/Scripts/ObjectController.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ObjectController : MonoBehaviour {
 
+	[SerializeField] private Text treeCount;
+
 	// Use this for initialization
 	public Transform lightPrefab;
 	public GameObject audioController;
+	public int treesDestroyed = 0;
+	public int treesTotal = 7; // TODO: automate this count instead of hard-code it
 
 	void Start () {
+		treeCount = GetComponent<Text> ();
+
 		audioController = GameObject.Find( "audioController" );
-	
+		treeCount.text = treesDestroyed + " / " + treesTotal;
 	}
 	
 	// Update is called once per frame
@@ -30,8 +37,5 @@ public class ObjectController : MonoBehaviour {
 			audioController.GetComponent<AudioController>().audio.Play ();
 			Destroy(gameObject);
 		}
-
-
 	}
-
 }
