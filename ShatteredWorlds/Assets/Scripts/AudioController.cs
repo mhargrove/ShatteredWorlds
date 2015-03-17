9c382c79a6eefe8446ce038e91d419810bb9574a
Colin Blaise <cblais3@lsu.edu>
2015-03-17 18:05:41 -0500

    adds ambient sounds to the project

--- ShatteredWorlds/Assets/Scripts/AudioController.cs
+++ ShatteredWorlds/Assets/Scripts/AudioController.cs
@@ -8,17 +8,29 @@ public class AudioController : MonoBehaviour {
 	//REMEMBER, you must drag the SFX onto the AudioController in your scene to hydrate the variable
 
 	public GameObject treeExplosionSfx;
+	public GameObject ambience2;
+	public GameObject ambience3;
 	// Use this for initialization
 	void Start () {
+		loopMusic ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 	}
 
 	public void playTreeExplosionSfx()
 	{
 		treeExplosionSfx.GetComponent<AudioSource> ().Play ();
+	}
+	public void loopMusic()
+	{
+		ambience2.GetComponent<AudioSource> ().Play ();
+		if (!ambience2.audio.isPlaying)
+			ambience3.GetComponent<AudioSource> ().Play ();
+
+
 	}
 
 }
