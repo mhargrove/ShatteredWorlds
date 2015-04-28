50f05beff7808997232d3592827459c733ce5405
Morgan Hargrove <mhargr5@lsu.edu>
2015-04-28 15:33:38 -0500

    fix play button

--- ShatteredWorlds/Assets/Scripts/MenuController.cs
+++ ShatteredWorlds/Assets/Scripts/MenuController.cs
@@ -3,14 +3,23 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
+	public GameObject arduinoController;
+	private int leftFoot;
+	private int rightFoot;
+
 	// Use this for initialization
 	void Start () {
-	
+		arduinoController = GameObject.Find ("ArduinoData");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		leftFoot = arduinoController.GetComponent<ArduinoController> ().readLeftFootpad (); 
+		rightFoot = arduinoController.GetComponent<ArduinoController> ().readRightFootpad ();
+
+		if (leftFoot == 1 || rightFoot == 1) {
+			LoadScene(2);
+		}
 	}
 
 	public void LoadScene(int level){
