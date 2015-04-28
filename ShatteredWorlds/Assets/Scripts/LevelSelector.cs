515a42411135598de187da40874d6977e945f9c5
Colin Blaise <cblais3@lsu.edu>
2015-04-28 16:54:39 -0500

    stuff

--- ShatteredWorlds/Assets/Scripts/LevelSelector.cs
+++ ShatteredWorlds/Assets/Scripts/LevelSelector.cs
@@ -11,7 +11,6 @@ public class LevelSelector : MonoBehaviour {
 		string Level1data = gameController.GetComponent<GameController> ().GetLevel1Statistics ();
 		string Level2data = gameController.GetComponent<GameController> ().GetLevel2Statistics ();
 		GameObject time1 = GameObject.Find ("Time1");
-		time1.
 	}
 	
 	// Update is called once per frame
